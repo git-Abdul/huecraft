ea445f3b020265fb7eddbd40ad3292e1f24483c7
Abdul <arpro7886@gmail.com>
2024-10-05 15:05:13 +0530

    Bug Fixes

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    images: {
-        unoptimized: true
+    experimental: {
+        images: {
+            unoptimized: true // Disables Next.js image optimization for all images.
+        }
     }
-}
-module.exports = nextConfig
+};
+
+module.exports = nextConfig;
